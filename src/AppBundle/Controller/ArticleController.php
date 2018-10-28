e1d75948193fdc4659ad5a1f519beb81764eb9c2
Kamel REMAKI <remakikamel@hotmail.fr>
2018-10-28 11:04:33 +0100

    add GuzzleHttpClient and base_uri as parameter in services.yml

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -1,14 +1,30 @@
 <?php
 namespace  AppBundle\Controller;
 
-
-
-
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
 
 class ArticleController extends Controller
 {
 
 
+    /**
+     * @Route("/",name="articles.index")
+     * @Method("GET")
+     */
+
+    public  function indexAction()
+    {
+
+
+
+
+
+    }
 
 
 
